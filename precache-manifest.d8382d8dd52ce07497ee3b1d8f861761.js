1073e1bbe60fe2c2e78b44a91865e8dbd284fbff
bincy90 <bincymargret@gmail.com>
2020-04-21 13:21:44 +1000

    Updates

---
+++ precache-manifest.d8382d8dd52ce07497ee3b1d8f861761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1957b6110b34d1175fb21a0502a450fd",
+    "url": "/expense/index.html"
+  },
+  {
+    "revision": "8304d28dafd28a56f21c",
+    "url": "/expense/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "e26a887dcf5bc08266e0",
+    "url": "/expense/static/js/2.6080d949.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense/static/js/2.6080d949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8304d28dafd28a56f21c",
+    "url": "/expense/static/js/main.eba89564.chunk.js"
+  },
+  {
+    "revision": "57556a849439c19a6ec7",
+    "url": "/expense/static/js/runtime-main.ec219e62.js"
+  }
+]);
